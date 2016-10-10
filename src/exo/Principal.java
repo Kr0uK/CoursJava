cb01ae5f177cb68c5c2a446858390f1a183d30af
Kr0uK <boris.motz@gmail.com>
2016-10-10 14:30:42 +0200

    ..

--- src/exo/Principal.java
+++ src/exo/Principal.java
@@ -4,18 +4,52 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
 
-    public enum ListeCouleurs{
+    public enum ListeCouleurs {
+        BLEUFONCE(1, "Bleu", "foncé"),
+        NOIR(2, , "noir", ""),
+        BLANC(3, "blanc", ""),
+        GRISFONCE(4, "gris", "foncé"),
+        BLEUCLAIR(5, "bleu", "clair"),
+        BLEU(6, "bleu", "");
 
+        public int getId() {
+            return id;
+        }
 
+        public String getCouleur() {
+            return couleur;
+        }
+
+        public String getNuance() {
+            return nuance;
+        }
+
+        ListeCouleurs(int id, String couleur, String nuance) {
+            this.id = id;
+            this.couleur = couleur;
+            this.nuance = nuance;
+        }
+
+        int id;
+        String couleur;
+        String nuance;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+       List<ListeCouleurs> ListCouleurs = new ArrayList<>();
+        for (ListeCouleurs couleur: ListeCouleurs.values()) {
+            
+        }
 
-		
+        //ListeCouleurs.add();
+
 
     }
 }
